1f6e6178b99308a6477cdacda300563e33a53139
Alex Coronell <alexcoronell@gmail.com>
2024-09-16 15:18:14 -0500

    Menu completed in mobile version

---
+++ src/app/components/icons/MdiMenuOpen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { SVGProps } from 'react';
+
+export function MdiMenuOpen(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M21 15.61L19.59 17l-5.01-5l5.01-5L21 8.39L17.44 12zM3 6h13v2H3zm0 7v-2h10v2zm0 5v-2h13v2z"></path></svg>);
+}
--- src/app/components/shared/Header.tsx
+++ src/app/components/shared/Header.tsx
@@ -1,5 +1,65 @@
+"use client"
+/* Components */
+import Link from "next/link"
+import Image from "next/image";
+import { useState } from "react";
+import { MdiMenuOpen } from "@/app/components/icons/MdiMenuOpen"
+import { MdiMenuClose } from "@/app/components/icons/MdiMenuClose"
+
+import styles from "@/css/header.module.css";
+
+import logo from '@/assets/images/logo/high-range-logo-orange-white.webp'
+
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const handleClick = (active: boolean) => setIsOpen(active);
+
+    const menu = [
+        {
+            title: "Home",
+            url: "/"
+        },
+        {
+            title: "Porsche",
+            url: "/porsche"
+        },
+        {
+            title: "Ferrary",
+            url: "/ferrary"
+        },
+        {
+            title: "Lamborghini",
+            url: "/lamborghini"
+        },
+        {
+            title: "Maserati",
+            url: "/maserati"
+        },
+        {
+            title: "Contacto",
+            url: "/contacto"
+        },
+    ]
     return (
-        <p>Header</p>
+        <header className={styles.Header}>
+            <Image src={logo} alt="Hight Race Logo" className="w-[150px]"  />
+            <MdiMenuOpen className="text-5xl text-orange-500" onClick={() => handleClick(true)} />
+
+            <nav className={isOpen ? "right-0 opacity-100" : "right-[-100%] opacity-0"}>
+                <ul>
+                <Image src={logo} alt="Hight Race Logo" className="w-[300px] mb-6"  />
+                    {
+                        menu.map((item, index) => (
+                            <li key={index}>
+                                <Link className="transition-all duration-300 hover:text-orange-500" href={item.url}>{item.title}</Link>
+                            </li>
+                        ))
+                    }
+                    
+                    <MdiMenuClose className="absolute right-3 top-3 text-5xl text-orange-500" onClick={() => handleClick(false)}  />
+                </ul>
+            </nav>
+        </header>
     )
 }
