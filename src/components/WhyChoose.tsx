67b6c8c174f97a67e055e3251bf2577ea8cb6c59
Alex Coronell <alexcoronell@gmail.com>
2024-09-17 19:09:30 -0500

    Whoo choose section completed

---
+++ src/components/WhyChoose.tsx
@@ -0,0 +1,14 @@
+import styles from "@/css/whyChoose.module.css";
+
+export default function WhyChoose() {
+    return (
+        <section className={styles.WhyChoose}>
+            <div className={styles.WhyChoose__content}>
+                <h1>High Range</h1>
+                <hr />
+                <p>Elegir Hight Range significa optar por vehículos de lujo de marcas prestigiosas como Porsche, Ferrari, Lamborghini y Maserati, garantizando calidad y exclusividad. Ofrecemos una experiencia de compra personalizada y un servicio postventa excepcional, además de la oportunidad de unirte a una comunidad de entusiastas del automóvil. En Hight Range, no solo adquieres un vehículo; vives una experiencia que combina pasión y un servicio inigualable.</p>
+                <hr />
+            </div>
+        </section>
+    )
+};
